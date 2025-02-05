73295b8eb8d5c9b63f0a5224df128dfe5b7e633b
Aaron Suarez <familyspeterson04@gmail.com>
2025-02-04 18:03:38 -0600

    update

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 # Import the dependencies.
 import pandas as pd
-from sqlalchemy import create_engine
+import sqlalchemy
+from sqlalchemy import create_engine, func
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from flask import Flask, jsonify
+from sql_helper import SQLHelper # isn't working/nothing wrong with flask
 
 #################################################
 # Database Setup
@@ -11,13 +13,15 @@ from flask import Flask, jsonify
 
 # Create engine using the `hawaii.sqlite` database file
 engine = create_engine('hawaii.sqlite')
+
 # Declare a Base using `automap_base()`
 Base = automap_base()
+
 # Use the Base class to reflect the database tables
 Base.prepare(engine, reflect=True)
 
 # Assign the measurement class to a variable called `Measurement` and
-# the station class to a variable called `Station`
+#the station class to a variable called `Station`
 measurement = Base.classes.measurement
 station = Base.classes.station
 
@@ -28,7 +32,18 @@ session = Session(engine)
 # Flask Setup
 #################################################
 app = Flask(__name__)
-
+sqlHelper = SQLHelper()
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def home():
+    return """/api/v1.0/precipitation 
+                /api/v1.0/stations
+                /api/v1.0/tobs
+                /api/v1.0/temp/<start>
+                /api/v1.0/temp/<start>/<end>
+            """ 
+
+
+
