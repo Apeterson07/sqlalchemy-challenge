a129c33bde52f106fd15db7baba3380dc5144bbd
Aaron Suarez <familyspeterson04@gmail.com>
2025-02-04 21:20:15 -0600

    update

--- app.py
+++ app.py
@@ -1,38 +1,35 @@
 # Import the dependencies.
 import pandas as pd
+import os
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 import sqlalchemy
 from sqlalchemy import create_engine, func
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
+
 from flask import Flask, jsonify
-from sql_helper import SQLHelper # isn't working/nothing wrong with flask
+from sql_helper import SQLHelper
 
-#################################################
-# Database Setup
-#################################################
 
-# Create engine using the `hawaii.sqlite` database file
-engine = create_engine('hawaii.sqlite')
+DB_PATH = 'sqlite:///Resources/hawaii.sqlite'
 
-# Declare a Base using `automap_base()`
-Base = automap_base()
 
-# Use the Base class to reflect the database tables
-Base.prepare(engine, reflect=True)
-
-# Assign the measurement class to a variable called `Measurement` and
-#the station class to a variable called `Station`
-measurement = Base.classes.measurement
-station = Base.classes.station
-
-# Create a session
-session = Session(engine)
+# #################################################
+# # Database Setup
+# #################################################
+# # Create engine using the `hawaii.sqlite` database file
+# engine = create_engine(DB_PATH)
+# Base = automap_base()
+# Base.prepare(engine, reflect=True)
+# measurement = Base.classes.measurement
+# station = Base.classes.station
+# session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
 app = Flask(__name__)
-sqlHelper = SQLHelper()
+sqlHelper = SQLHelper(DB_PATH)
 #################################################
 # Flask Routes
 #################################################
@@ -44,6 +41,60 @@ def home():
                 /api/v1.0/temp/<start>
                 /api/v1.0/temp/<start>/<end>
             """ 
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Execute Query
+    df = sqlHelper.queryPrecipitationSQL()
+
+    # Turn DataFrame into List of Dictionary
+    data = df.to_dict(orient="records")
+    return jsonify(data)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    # Execute Query
+    df = sqlHelper.queryStationsSQL()
+
+    # Turn DataFrame into List of Dictionary
+    data = df.to_dict(orient="records")
+    return jsonify(data)
+
+@app.route("/api/v1.0/tobs")
+def temperature():
+    # Execute Query
+    df = sqlHelper.queryTempSQL()
+
+    # Turn DataFrame into List of Dictionary
+    data = df.to_dict(orient="records")
+    return jsonify(data)
+
+@app.route("/api/v1.0/<start>")
+def tstats1(start):
+    # Execute Query
+    df = sqlHelper.queryTStatsSQL(start)
+
+    # Turn DataFrame into List of Dictionary
+    data = df.to_dict(orient="records")
+    return jsonify(data)
+
+@app.route("/api/v1.0/<start>/<end>")
+def tstats_se1(start, end):
+    # Execute Query
+    df = sqlHelper.queryTstats_SESQL(start, end)
+
+    # Turn DataFrame into List of Dictionary
+    data = df.to_dict(orient="records")
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+
 
 
 
